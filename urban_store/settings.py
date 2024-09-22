0c6bf96b3aba54f33faeb98cd3ccf223b7152251
yeemaae <noobproger@gmail.com>
2024-09-22 08:25:15 +0500

    Connected to PostgreSQL

--- urban_store/settings.py
+++ urban_store/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'urban_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'urbanstore',
+        'USER': 'store_admin',
+        'PASSWORD': secret_settings.store_pass,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
