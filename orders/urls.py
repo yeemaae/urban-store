e60dfb37a6d1ea2fc1895dc8982a25ba948edcb7
yeemaae <noobproger@gmail.com>
2024-09-29 08:05:22 +0500

    Added webhook

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import OrderCreateView, SuccessTemplateView, CanceledTemplateView
+from .views import CanceledTemplateView, OrderCreateView, SuccessTemplateView
 
 app_name = 'orders'
 
--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,17 @@
-import stripe
-
+import json
 from http import HTTPStatus
-from django.http import HttpResponseRedirect
-from django.views.generic.edit import CreateView
-from django.views.generic.base import TemplateView
-from django.urls import reverse_lazy, reverse
+
+import stripe
 from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+
+from common.views import TitleMixin
 
 from .forms import OrderForm
-from common.views import TitleMixin
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -38,6 +41,7 @@ class OrderCreateView(TitleMixin, CreateView):
                     'quantity': 1,
                 },
             ],
+            metadata={'order_id': self.object.id},
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
             cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_canceled')),
@@ -47,3 +51,32 @@ class OrderCreateView(TitleMixin, CreateView):
     def form_valid(self, form):
         form.instance.initiator = self.request.user
         return super(OrderCreateView, self).form_valid(form)
+
+
+# Using Django
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        fulfill_order(session)
+
+    return HttpResponse(status=200)
+
+
+def fulfill_order(session):
+    print("Fulfilling Checkout Session", session)
